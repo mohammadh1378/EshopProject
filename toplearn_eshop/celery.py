c72535c58fe6f875597844a0aaf18ead17e00330
mohammadh1378 <mohammad.hemmatnia@gmail.com>
2020-09-20 10:58:08 +0430

    celery updated

--- toplearn_eshop/celery.py
+++ toplearn_eshop/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toplear_eshop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toplearn_eshop.settings')
 app = Celery('toplearn_eshop')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
